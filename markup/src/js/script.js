6160ff420db613f67702cd917d15a1bc932e3191
Maxim Safronov <1maximsafronov@gmail.com>
2022-04-16 15:23:53 +0700

    Добавляет стилизацию для мобилной шапки

--- markup/src/js/script.js
+++ markup/src/js/script.js
@@ -1,3 +1,12 @@
 // входная точка для сборки js файлов
 
 console.log(`Скрипты подключены`);
+
+
+const pageNav = document.querySelector(`.page-header__nav`);
+const navToggle = pageNav.querySelector(`.page-header__nav-toggle`);
+
+navToggle.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  pageNav.classList.toggle(`page-header__nav--closed`);
+})
